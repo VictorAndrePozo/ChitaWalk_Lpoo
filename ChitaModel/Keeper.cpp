b41aa2c4b6aa81b2ab039c1b21228b5c9dedb50e
Talita Ricaldi <talita.ricaldi@pucp.edu.pe>
2022-09-25 17:16:05 -0500

    KEEPER_ACTUALIZADO

--- ChitaModel/Keeper.cpp
+++ ChitaModel/Keeper.cpp
@@ -1,6 +1,3 @@
-/**
- * Project Untitled
- */
 #include "pch.h"
 
 #include "Keeper.h"
@@ -9,6 +6,29 @@ ChitaiModel::Keeper::Keeper() {
 
 }
 
-ChitaiModel::Keeper::Keeper(bool Home) {
+ChitaiModel::Keeper::Keeper(int Dni, String^ DocNumbrer, String^ Address, String^ PhoneNumber,
+	String^ Email, char Gender, int Id, String^ username,
+	String^ password, String^ Lastname, String^ Firstname,
+	int Valoration, String^ Award, int Experience,
+	int Level, int Warranty, double Salary, Disponibility^ objDisponibility,
+	bool Home) {
+	this->Dni = Dni;
+	this->DocNumbrer = DocNumbrer;
+	this->Address = Address;
+	this->PhoneNumber = PhoneNumber;
+	this->Email = Email;
+	this->Gender = Gender;
+	this->Id = Id;
+	this->username = username;
+	this->password = password;
+	this->Lastname = Lastname;
+	this->Firstname = Firstname;
+	this->Valoration = Valoration;
+	this->Award = Award;
+	this->Experience = Experience;
+	this->Level = Level;
+	this->Warranty = Warranty;
+	this->Salary = Salary;
+	this->objDisponibility = objDisponibility;
 	this->Home = Home;
 }
--- ChitaModel/Keeper.h
+++ ChitaModel/Keeper.h
@@ -1,13 +1,19 @@
 #pragma once
+#include "Carer.h"
 
 using namespace System;
 
 namespace ChitaiModel {
-	public ref class Keeper{
+	public ref class Keeper : public Carer {
 	public:
 		property bool Home;
 	public:
 		Keeper();
-		Keeper(bool Home);
+		Keeper(int Dni, String^ DocNumbrer, String^ Address, String^ PhoneNumber,
+			String^ Email, char Gender, int Id, String^ username,
+			String^ password, String^ Lastname, String^ Firstname,
+			int Valoration, String^ Award, int Experience,
+			int Level, int Warranty, double Salary, Disponibility^ objDisponibility,
+			bool Home);
 	};
 }
