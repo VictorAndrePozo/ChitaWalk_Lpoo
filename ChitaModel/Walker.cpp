ac886dae8deb685c1de3366d5139415feac29bdb
Cristhian <a20195781@pucp.edu.pe>
2022-09-25 17:15:34 -0500

    CAMBIOS DE WALKER.CPP Y WALKER.H

--- ChitaModel/Walker.cpp
+++ ChitaModel/Walker.cpp
@@ -1,6 +1,3 @@
-/**
- * Project Untitled
- */
 #include "pch.h"
 
 #include "Walker.h"
@@ -9,6 +6,28 @@ ChitaiModel::Walker::Walker() {
 
 }
 
-ChitaiModel::Walker::Walker(String^ StrollArea) {
+ChitaiModel::Walker::Walker(int Dni, String^ DocNumbrer, String^ Address, String^ PhoneNumber,
+	String^ Email, char Gender, int Id, String^ username,
+	String^ password, String^ Lastname,
+	String^ Firstname, int Valoration, String^ Award, int Experience,
+	int Level, int Warranty, double Salary, Disponibility^ objDisponibility, String^ StrollArea) {
+	this->Dni = Dni;
+	this->DocNumbrer = DocNumbrer;
+	this->Address = Address;
+	this->PhoneNumber = PhoneNumber;
+	this->Email = Email;
+	this->Gender = Gender;
+	this->Id = Id;
+	this->username = username;
+	this->password = password;
+	this->Lastname = Lastname;
+	this->Firstname = Firstname;
+	this->Valoration = Valoration;
+	this->Award = Award;
+	this->Experience = Experience;
+	this->Level = Level;
+	this->Warranty = Warranty;
+	this->Salary = Salary;
+	this->objDisponibility = objDisponibility;
 	this->StrollArea = StrollArea;
 }
--- ChitaModel/Walker.h
+++ ChitaModel/Walker.h
@@ -1,13 +1,19 @@
 #pragma once
 
+#include "Carer.h"
+
 using namespace System;
 
 namespace ChitaiModel {
-	public ref class Walker{
+	public ref class Walker : public Carer {
 	public:
 		property String^ StrollArea;
 	public:
 		Walker();
-		Walker(String^ StrollArea);
+		Walker(int Dni, String^ DocNumbrer, String^ Address, String^ PhoneNumber,
+			String^ Email, char Gender, int Id, String^ username,
+			String^ password, String^ Lastname,
+			String^ Firstname, int Valoration, String^ Award, int Experience,
+			int Level, int Warranty, double Salary, Disponibility^ objDisponibility, String^ StrollArea);
 	};
 }
