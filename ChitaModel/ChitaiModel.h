a7fb488a91f72170c01f38403597ef3971b56175
Cristhian <a20195781@pucp.edu.pe>
2022-09-25 23:58:43 -0500

    Model Clases modificado: ya se termino, solo falta la relacion toxica de achievment

---
+++ ChitaModel/ChitaiModel.h
@@ -0,0 +1,13 @@
+#pragma once
+#pragma once
+
+using namespace System;
+
+namespace Chitai {
+	public ref class Class1
+	{
+		// TODO: Agregue aquí los métodos de esta clase.
+		// (Torrejita) => en el proyecto del profe esta estos archivos .h .cpp
+		// lo pongo por si acaso.
+	};
+}
