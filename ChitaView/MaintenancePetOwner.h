426c6fa8ddec39cc5f4141f9f72ea86750a1821b
Cristhian <a20195781@pucp.edu.pe>
2022-09-29 13:09:00 -0500

    a

--- ChitaView/MaintenancePetOwner.h
+++ ChitaView/MaintenancePetOwner.h
@@ -46,16 +46,16 @@ namespace ChitaView {
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Valoration;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Username;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Apellido;
-	private: System::Windows::Forms::TextBox^ textBox1;
+
 	private: System::Windows::Forms::TextBox^ textBox2;
-	private: System::Windows::Forms::Label^ label1;
+
 	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Label^ label3;
+
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::Label^ label5;
 	private: System::Windows::Forms::TextBox^ textBox3;
 	private: System::Windows::Forms::TextBox^ textBox4;
-	private: System::Windows::Forms::TextBox^ textBox5;
+
 
 
 	private:
@@ -80,16 +80,12 @@ namespace ChitaView {
 			this->Valoration = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Username = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Apellido = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -158,51 +154,26 @@ namespace ChitaView {
 			this->Apellido->HeaderText = L"Apellido";
 			this->Apellido->Name = L"Apellido";
 			// 
-			// textBox1
-			// 
-			this->textBox1->Location = System::Drawing::Point(132, 53);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
-			this->textBox1->TabIndex = 4;
-			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(132, 87);
+			this->textBox2->Location = System::Drawing::Point(132, 25);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(100, 20);
 			this->textBox2->TabIndex = 5;
 			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(45, 53);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(26, 13);
-			this->label1->TabIndex = 6;
-			this->label1->Text = L"Id(*)";
-			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(45, 140);
+			this->label2->Location = System::Drawing::Point(45, 61);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(54, 13);
 			this->label2->TabIndex = 7;
 			this->label2->Text = L"Nombre(*)";
 			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(45, 217);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(64, 13);
-			this->label3->TabIndex = 8;
-			this->label3->Text = L"Valoration(*)";
-			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(45, 94);
+			this->label4->Location = System::Drawing::Point(44, 28);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(65, 13);
 			this->label4->TabIndex = 9;
@@ -211,7 +182,7 @@ namespace ChitaView {
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(45, 180);
+			this->label5->Location = System::Drawing::Point(45, 95);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(54, 13);
 			this->label5->TabIndex = 10;
@@ -219,40 +190,29 @@ namespace ChitaView {
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(132, 137);
+			this->textBox3->Location = System::Drawing::Point(132, 58);
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(100, 20);
 			this->textBox3->TabIndex = 11;
 			// 
 			// textBox4
 			// 
-			this->textBox4->Location = System::Drawing::Point(132, 180);
+			this->textBox4->Location = System::Drawing::Point(132, 92);
 			this->textBox4->Name = L"textBox4";
 			this->textBox4->Size = System::Drawing::Size(100, 20);
 			this->textBox4->TabIndex = 12;
-			// 
-			// textBox5
-			// 
-			this->textBox5->Location = System::Drawing::Point(132, 217);
-			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(100, 20);
-			this->textBox5->TabIndex = 13;
 			// 
 			// MaintenancePetOwner
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(567, 474);
-			this->Controls->Add(this->textBox5);
 			this->Controls->Add(this->textBox4);
 			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
-			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
 			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
