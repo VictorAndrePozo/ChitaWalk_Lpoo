36251e5adb5c6703c0a74c48e49f779055adadaf
Talita Ricaldi <talita.ricaldi@pucp.edu.pe>
2022-09-29 13:53:17 -0500

    Controller para mantenimiento de carer

--- ChitaController/ChitaController.cpp
+++ ChitaController/ChitaController.cpp
@@ -2,3 +2,52 @@
 
 #include "ChitaController.h"
 
+
+//---------------------------------------------------------------------------------------------------------------------
+// PARA MANTENIMIENTO DE CARER
+//---------------------------------------------------------------------------------------------------------------------
+int ChitaController::Controller::AddCarer(Carer^ carer)
+{
+	carerList->Add(carer);
+	return 1;
+}
+
+int ChitaController::Controller::UpdateCarer(Carer^ carer)
+{
+	for (int i = 0; i < carerList->Count; i++)
+		if (carer->Id == carerList[i]->Id) {
+			carerList[i] = carer;
+			return 1;
+		}
+	return 0;
+}
+
+int ChitaController::Controller::DeleteCarer(int carerId)
+{
+	for (int i = 0; i < carerList->Count; i++)
+		if (carerId == carerList[i]->Id) {
+			carerList->RemoveAt(i);
+			return 1;
+		}
+	return 0;
+}
+
+List<Carer^>^ ChitaController::Controller::QueryAllCarers()
+{
+	List<Carer^>^ activeCarersList = gcnew List<Carer^>();
+	for (int i = 0; i < carerList->Count; i++) {
+		if (carerList[i]->Status == 'A') {
+			activeCarersList->Add(carerList[i]);
+		}
+	}
+	return activeCarersList;
+}
+
+Carer^ ChitaController::Controller::QueryCarerById(int carerId)
+{
+	for (int i = 0; i < carerList->Count; i++)
+		if (carerId == carerList[i]->Id) {
+			return carerList[i];
+		}
+	return nullptr;
+}
--- ChitaController/ChitaController.h
+++ ChitaController/ChitaController.h
@@ -1,10 +1,23 @@
 #pragma once
 
 using namespace System;
+using namespace ChitaModel;
+using namespace System::Collections::Generic;
 
 namespace ChitaController {
-	public ref class Class1
+	public ref class Controller
 	{
-		// TODO: Agregue aquí los métodos de esta clase.
+		//---------------------------------------------------------------------------------------------------------------------
+		// PARA MANTENIMIENTO DE CARER
+		//---------------------------------------------------------------------------------------------------------------------
+
+	private:
+		static List <Carer^>^ carerList = gcnew List<Carer^>(); // Variable de clase o atributo estático
+	public:
+		static int AddCarer(Carer^ carer);                      // Añadir carer
+		static int UpdateCarer(Carer^ carer);                   // Modificar carer
+		static int DeleteCarer(int carerId);                    // Eliminar carer
+		static List <Carer^>^ QueryAllCarers();                 // Consultar todos los Carers activos
+		static Carer^ QueryCarerById(int carerId);              // Consultar Carer por ID
 	};
 }
