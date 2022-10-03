7fbf0f0a002b17ccb83e5c498ce71509c000ddf6
victorpozo <a20195974@pucp.edu.pe>
2022-10-02 19:01:06 -0500

    Pet Controller v1 done

--- ChitaController/ChitaController.cpp
+++ ChitaController/ChitaController.cpp
@@ -51,3 +51,55 @@ Carer^ ChitaController::Controller::QueryCarerById(int carerId)
 		}
 	return nullptr;
 }
+
+//---------------------------------------------------------------------------------------------------------------------
+// PARA MANTENIMIENTO DE CARER
+//---------------------------------------------------------------------------------------------------------------------
+int ChitaController::Controller::AddPet(Pet^ pet)
+{
+    petList->Add(pet);  //agregar a la petList, el pet que entra al método
+    return 0;
+}
+
+int ChitaController::Controller::UptadePet(Pet^ pet)
+{
+    for (int i = 0; i < petList->Count; i++) { // de 0 a ++ mientras i sea menor al total de listas(count)
+        if (pet->Id == petList[i]->Id) {    //Si atributo Id de Pet es igual al atributo Id de petList entonces...
+            petList[i] = pet;   //Flag Done 
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int ChitaController::Controller::DeletePet(int petId)
+{
+    for (int i = 0; i < petList->Count; i++) { // de 0 a ++ mientras i sea menor al total de listas(count)
+        if (petId == petList[i]->Id) {    //Si petId entrante es igual al atributo Id de petList entonces...
+            petList->RemoveAt(i);       //Borrar toda la celda perteneciente a dicho i (Id)
+            return 1;   //Flag Done
+        }
+    }
+    return 0;
+}
+
+Pet^ ChitaController::Controller::QueryPoductById(int petId)
+{
+    for (int i = 0; i < petList->Count; i++) { //Barrido de todos los elementos de la lista
+        if (petId == petList[i]->Id) {    //Si petId es igual al atributo Id de petList entonces...
+            return petList[i];      //Retornamos la lista si encuentra una coincidencia
+        }
+    }
+    return nullptr; //Si no encuentra una coincidenica, no debuelve algo.
+}
+
+List<Pet^>^ ChitaController::Controller::QueryAllPets()
+{
+    List<Pet^>^ activePetList = gcnew List<Pet^>(); //Reserva espacio de memoria para la lista
+    for (int i = 0; i < petList->Count; i++) {  //Barrido de todos los elementos de la lista
+        if (petList[i]->Status == 'w') {    //status char: esperando(w) servicios care (c), walk(k), transport(t)
+            activePetList->Add(petList[i]);
+        }
+    }
+    return activePetList;
+}
--- ChitaController/ChitaController.h
+++ ChitaController/ChitaController.h
@@ -19,5 +19,21 @@ namespace ChitaController {
 		static int DeleteCarer(int carerId);                    // Eliminar carer
 		static List <Carer^>^ QueryAllCarers();                 // Consultar todos los Carers activos
 		static Carer^ QueryCarerById(int carerId);              // Consultar Carer por ID
+
+		//---------------------------------------------------------------------------------------------------------------------
+		// PARA MANTENIMIENTO DE PET
+		//---------------------------------------------------------------------------------------------------------------------
+	
+	private:	//privado para que otras clases no accedan a el.
+		static List <Pet^>^ petList = gcnew List<Pet^>();	//como atributo para que viva mientras controller esté activo. static: Lista unica y global.
+
+	public: //static es para que los metodos sean globales.
+		static int AddPet(Pet^ pet);				//Método para añadir mascotas.
+		static int UptadePet(Pet^ pet);				//Método para actualizar datos de las mascotas.
+		static int DeletePet(int productId);		//Método para eliminar una mascota del registro.
+
+		static Pet^ QueryPoductById(int petId);		//Método para mostrar una mascota por ID.
+		static List <Pet^>^ QueryAllPets();			//Método para mostrar a todas las mascotas en una lista.
+
 	};
 }
